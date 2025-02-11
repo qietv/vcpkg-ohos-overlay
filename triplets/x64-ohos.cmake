6d4c5bffc829aa3355ef0a2cc1ff7cbc6744f5f7
MouriNaruto <Kenji.Mouri@outlook.com>
2025-02-11 11:49:04 +0800

    Add triplets\x64-ohos.cmake.

---
+++ triplets/x64-ohos.cmake
@@ -0,0 +1,17 @@
+﻿## 
+## PROJECT:   The vcpkg overlay ports and triplets for OpenHarmony Native SDK
+## FILE:      x64-ohos.cmake
+## PURPOSE:   The vcpkg overlay triplet for x64 OpenHarmony Native SDK.
+## 
+## LICENSE:   The MIT License
+## 
+## MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+## 
+
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/ohos-sdk-native.cmake")
